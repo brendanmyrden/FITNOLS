465ca40e3b4fe58a786ee11369704e607eb98b71
MYRDEN <79550484+brendanmyrden@users.noreply.github.com>
2025-09-30 20:55:46 -0300

    Basic structure of the project

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FitnolsApp from "./FitnolsApp.jsx";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FitnolsApp />
+  </React.StrictMode>
+);
+
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FitnolsApp from "./FitnolsApp.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FitnolsApp />
+  </React.StrictMode>
+);
